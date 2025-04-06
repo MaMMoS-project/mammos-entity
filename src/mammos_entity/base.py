647c600c7a838faa50a5cce7e729d8a631bfe0dc
Swapneel Amit Pathak <swapiitr@gmail.com>
2025-04-06 10:30:13 +0200

    fix(base): revert the use of atropy SI units
    This converts the J/m to N and J/m3 to Pa: which is OK since it is
    trying to convert the units to SI but it is at odds with the ontology.

--- src/mammos_entity/base.py
+++ src/mammos_entity/base.py
@@ -117,7 +117,7 @@ class Entity(u.Quantity):
             raise TypeError(
                 f"{label} is a unitless entity. Hence, {unit} is inapropriate."
             )
-        comp_unit = u.Unit(unit if unit else "").si
+        comp_unit = u.Unit(unit if unit else "")
         comp_bases = comp_unit.bases
         comp_powers = comp_unit.powers
         return super().__new__(
