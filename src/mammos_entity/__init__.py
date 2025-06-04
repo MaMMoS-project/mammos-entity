6c6d8f0f7ae3f900ab44ed8e08597d4f3cf4799c
Martin Lang <martin.lang@mpsd.mpg.de>
2025-06-04 11:02:05 +0200

    Switch back to __all__; otherwise we get incomplete documentation

--- src/mammos_entity/__init__.py
+++ src/mammos_entity/__init__.py
@@ -8,18 +8,25 @@ loaded MaMMoS ontology object.
 
 import importlib.metadata
 
-from mammos_entity._base import Entity as Entity
-from mammos_entity._entities import A as A
-from mammos_entity._entities import B as B
-from mammos_entity._entities import BHmax as BHmax
-from mammos_entity._entities import H as H
-from mammos_entity._entities import Hc as Hc
-from mammos_entity._entities import Ku as Ku
-from mammos_entity._entities import M as M
-from mammos_entity._entities import Mr as Mr
-from mammos_entity._entities import Ms as Ms
-from mammos_entity._entities import T as T
-from mammos_entity._entities import Tc as Tc
-from mammos_entity._onto import mammos_ontology as mammos_ontology
+from mammos_entity._base import Entity
+from mammos_entity._entities import A, B, BHmax, H, Hc, Ku, M, Mr, Ms, T, Tc
+from mammos_entity._onto import mammos_ontology
 
 __version__ = importlib.metadata.version(__package__)
+
+
+__all__ = [
+    "Entity",
+    "A",
+    "B",
+    "BHmax",
+    "H",
+    "Hc",
+    "Ku",
+    "M",
+    "Mr",
+    "Ms",
+    "T",
+    "Tc",
+    "mammos_ontology",
+]
