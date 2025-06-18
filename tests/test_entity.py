cee45d3dfdad6d0d7319eda4e501337d42b719a4
Martin Lang <martin.lang@mpsd.mpg.de>
2025-06-18 18:26:00 +0200

    Extend tests to ensure correct subclassing from Quantity
    - check that a broad spectrum of functionality works
    - check that the correct type, Entity or Quantity, is returned based on context

--- tests/test_entity.py
+++ tests/test_entity.py
@@ -119,3 +119,140 @@ def test_wrong_quantity_with_equivalency_early_conversion():
     with u.set_enabled_equivalencies(u.magnetic_flux_field()):
         e = me.Ms(val.to(u.A / u.m))
         assert u.allclose(e.quantity, val)
+
+
+# various tests to check different numpy functions:
+# - return an entity if the meaning of the data has not changed and the units match
+# - return a quantity if something has changed (or if there is possible ambiguity)
+# type checks via `type(...) is ...` to ensure we check for exact type (not subclasses)
+
+
+def test_numpy_squeeze():
+    inp = me.Ms([[1, 1]])
+    out = np.squeeze(inp)
+    assert type(out) is me.Entity
+    assert out.ontology_label == inp.ontology_label
+    assert out.shape == (2,)
+    np.testing.assert_allclose(out.value, [1, 1])
+
+
+def test_numpy_power():
+    inp = me.Ms(3)
+    out = np.power(inp, 2)
+    assert type(out) is u.Quantity
+    np.testing.assert_allclose(out.value, 9)
+
+
+def test_numpy_ones_like():
+    inp = me.Ms(3)
+    out = np.ones_like(inp)
+    assert type(out) is me.Entity
+    assert out.ontology_label == inp.ontology_label
+    assert out.shape == ()
+    np.testing.assert_allclose(out.value, 1)
+
+
+def test_getitem():
+    inp = me.Ms([1, 2, 3])
+    out = inp[:]
+    assert type(out) is me.Entity
+    assert out.ontology_label == inp.onology_label
+    np.testing.assert_allclose(inp, out)
+
+    out = inp[[True, False, True]]
+    assert type(out) is me.Entity
+    assert out.ontology_label == inp.onology_label
+    np.testing.assert_allclose(inp, [1, 3])
+
+    out = inp[1]
+    assert type(out) is me.Entity
+    assert out.ontology_label == inp.onology_label
+    np.testing.assert_allclose(inp, 2)
+
+
+def test_np_positive():
+    inp = me.Ms(-100)
+    out = np.positive(inp)
+    assert type(out) is u.Quantity
+
+
+def test_isnan():
+    inp = me.Ms()
+    assert not np.isnan(inp)
+
+
+def test_to_string():
+    with pytest.raises(AttributeError):
+        me.Ms().to_string()
+
+
+def test_multiplication_with_number():
+    inp = me.Ms(1)
+    out = inp * 3
+    assert type(out) is u.Quantity
+    np.testing.assert_allclose(out.value, 3)
+
+
+def test_multiplication_with_unit():
+    inp = me.Ms(1)
+    out = inp * u.K
+    assert type(out) is u.Quantity
+    assert out.unit == "K A / m"
+
+
+def test_np_insert():
+    inp = me.Ms([1, 2])
+    out = inp.insert(0, 3 * u.A / u.m)
+    assert type(out) is me.Entity
+    assert out.ontology_label == inp.onology_label
+    np.testing.assert_allclose(inp.value, [3, 1, 2])
+
+
+def test_np_mean():
+    inp = me.Ms([1, 3])
+    out = np.mean(inp)
+    assert type(out) is u.Quantity
+    np.testing.assert_allclose(out.value, 2)
+
+
+def test_np_linalg_norm():
+    inp = me.Ms([3, 4])
+    out = np.linalg.norm(inp)
+    assert type(out) is u.Quantity
+    np.testing.assert_allclose(out.value, 5)
+
+
+def test_decompose():
+    inp = me.B()
+    out = inp.decompose()
+    assert type(out) is me.Entity
+    assert out.ontology_label == inp.ontology_label
+    assert out.unit == "kg / (A s2)"
+
+
+def test_lshift():
+    # unit conversion in astropy, implementation not well suited for subclassing
+    inp = me.B()
+    out = inp << u.mT
+    assert type(out) is me.Entity
+    assert out.ontology_label == inp.ontology_label
+
+    with (
+        u.set_enabled_equivalencies(u.magnetic_flux_field()),
+        pytest.raises(u.UnitConversionError),
+    ):
+        inp << u.A / u.m  # not compatible with ontology
+
+
+def test_ilshift():
+    val = me.B()
+    ontology_label = val.ontology_label
+    val <<= u.mT
+    assert type(val) is me.Entity
+    assert val.ontology_label == ontology_label
+
+    with (
+        u.set_enabled_equivalencies(u.magnetic_flux_field()),
+        pytest.raises(u.UnitConversionError),
+    ):
+        val <<= u.A / u.m  # not compatible with ontology
