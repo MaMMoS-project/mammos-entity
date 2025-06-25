91afa401563a7800c728a9914e25bdfa6a4fe348
Andrea Petrocchi <andrea.petrocchi@mpsd.mpg.de>
2025-06-25 13:55:30 +0200

    test: delete unused conftest

--- tests/conftest.py
+++
@@ -1,8 +0,0 @@
-import pytest
-
-import mammos_entity as me
-
-
-@pytest.fixture(scope="session")
-def onto_class_list():
-    return [str(cls.prefLabel[0]) for cls in me.mammos_ontology.classes(imported=True)]
