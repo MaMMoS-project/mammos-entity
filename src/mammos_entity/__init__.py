8cb234cea315c19c809e6866b742dd6a3570aff7
Andrea Petrocchi <andrea.petrocchi@mpsd.mpg.de>
2025-07-30 11:26:17 +0200

    feat: concat function to concatenate entities

--- src/mammos_entity/__init__.py
+++ src/mammos_entity/__init__.py
@@ -11,6 +11,7 @@ import importlib.metadata
 from mammos_entity._base import Entity
 from mammos_entity._entities import A, B, BHmax, H, Hc, J, Js, Ku, M, Mr, Ms, T, Tc
 from mammos_entity._onto import mammos_ontology
+from mammos_entity.operations import concat_flat
 
 from . import io, typing
 
@@ -32,6 +33,7 @@ __all__ = [
     "Ms",
     "T",
     "Tc",
+    "concat_flat",
     "mammos_ontology",
     "io",
     "typing",
