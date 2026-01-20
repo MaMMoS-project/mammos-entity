953e887ae2ebf5d374df9cc8e809a5122bfa36a9
Andrea Petrocchi <andrea.petrocchi@mpsd.mpg.de>
2026-01-20 13:30:55 +0100

    implement `search_labels` function
    Co-authored-by: Martin Lang <67915889+lang-m@users.noreply.github.com>

--- src/mammos_entity/__init__.py
+++ src/mammos_entity/__init__.py
@@ -13,7 +13,7 @@ import mammos_units as units
 from mammos_entity._entity import Entity
 from mammos_entity._entity_collection import EntityCollection
 from mammos_entity._factory import A, B, BHmax, H, Hc, J, Js, Ku, M, Mr, Ms, T, Tc
-from mammos_entity._ontology import mammos_ontology
+from mammos_entity._ontology import mammos_ontology, search_labels
 
 from . import io, operations
 
@@ -39,5 +39,6 @@ __all__ = [
     "io",
     "mammos_ontology",
     "operations",
+    "search_labels",
     "units",
 ]
