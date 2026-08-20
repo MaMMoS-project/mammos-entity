616b4dc17f9e82e09eb84eb3e8bb24fbbda18d60
Andrea Petrocchi <andrea.petrocchi@mpsd.mpg.de>
2026-08-20 10:24:36 +0200

    move hdf5 writing function to `_io` module

--- src/mammos_entity/_io/__init__.py
+++ src/mammos_entity/_io/__init__.py
@@ -5,7 +5,7 @@ from ._csv_v2 import _from_csv_v2
 from ._csv_v3 import _from_csv_v3
 from ._csv_v4 import _from_csv_v4, _to_csv_v4
 from ._hdf5_v1 import _from_hdf5_v1
-from ._hdf5_v2 import _from_hdf5_v2
+from ._hdf5_v2 import _from_hdf5_v2, _to_hdf5_v2
 from ._yaml_v1 import _from_yaml_v1
 from ._yaml_v2 import _from_yaml_v2
 from ._yaml_v3 import _from_yaml_v3, _to_yaml_v3
@@ -21,5 +21,6 @@ __all__ = [
     "_from_hdf5_v1",
     "_from_hdf5_v2",
     "_to_csv_v4",
+    "_to_hdf5_v2",
     "_to_yaml_v3",
 ]
