c974a7d5754cbafb82f8aa53400b3ba611bc0bad
Andrea Petrocchi <andrea.petrocchi@mpsd.mpg.de>
2026-08-31 14:27:40 +0200

    use `get_preferred_label()` instead of `.prefLabel` directly
    This excludes ontology things that are not classes but for example
    individuals (e.g. `AndreaPetrocchi`, `EMMC_ASBL`, `MaMMoS`)

--- src/mammos_entity/_ontology.py
+++ src/mammos_entity/_ontology.py
@@ -182,7 +182,9 @@ class Ontology:
         match_by_prefLabel = set(self._ontopy_ontology.search(prefLabel=label))
         match_by_altLabel = set(self._ontopy_ontology.search(altLabel=label))
         possible_things = match_by_label | match_by_prefLabel | match_by_altLabel
-        return sorted(str(thing.prefLabel[0]) for thing in possible_things if hasattr(thing, "prefLabel"))
+        return sorted(
+            str(thing.get_preferred_label()) for thing in possible_things if hasattr(thing, "get_preferred_label")
+        )
 
     def Entity(
         self,
